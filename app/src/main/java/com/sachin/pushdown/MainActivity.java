2c9e21c0de46a4574d8d2ca8abea15ecd1e8dcfa
Sachin143p <sachinpandey614@gmail.com>
2018-08-18 17:47:53 +0530

    Initial commit

---
+++ app/src/main/java/com/sachin/pushdown/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sachin.pushdown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void PushDown(){
+        int i = 2-1;
+    }
+}
